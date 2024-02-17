eb6ec3f254f0d5b7b7024a6a6753bcf6f4b361fd
Shreya-454 <bshreya932@GMAIL.COM>
2024-02-17 15:27:03 +0530

    px

--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -3,14 +3,14 @@ import Button from './Button'
 import w4 from '../assets/images/w4.webp'
 const Ready = () => {
   return (
-    <div className='relative lg:pb-[89px] lg:pt-[124px] md:py-10 py-5'>
-      <div className="max-w-[1140px] mx-auto ">
+    <div className='relative lg:pb-[89px] lg:pt-[124px] md:py-10 py-5 '>
+      <div className="max-w-[1140px] mx-auto  max-xl:px-3">
         <div className='flex flex-row flex-wrap -mx-3 '>
           <div className=' lg:w-1/2 w-full px-3'>
             <h2 data-aos="fade-right" className='font-recharge md:text-[40px] sm:text-4xl text-3xl font-bold md:leading-[52px] text-white max-w-[517px] max-lg:text-center max-lg:mx-auto'>Ready to get started
             <span className='text-[#02CDCF]'> Talk to us Today</span> </h2>
           </div>
-          <div className=' lg:w-1/2 w-full items-center flex lg:justify-end justify-center lg:mt-0 md:mt-10 mt-5'>
+          <div className=' lg:w-1/2 px-3 w-full items-center flex lg:justify-end justify-center lg:mt-0 md:mt-10 mt-5'>
             <Button text="Get Started"/>
           </div>
         </div>
