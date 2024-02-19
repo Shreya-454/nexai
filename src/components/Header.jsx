1e6743c29a4e258127075d639403b30da71e36f1
Shreya-454 <bshreya932@GMAIL.COM>
2024-02-19 09:43:59 +0530

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
           />
         </div>
         <img src={gradient} alt="gr" className="absolute bottom-[-5%] left-0 right-0" />
-        <img src={logo} alt="logo" className="absolute xl:right-[14%] xl:top-[37.5%] hidden xl:block"/>
+        <img src={logo} alt="logo" className="absolute xl:right-[14.3%] xl:top-[37.5%] hidden xl:block"/>
       </div>
     </>
   );
