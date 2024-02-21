b7abac7eb86e5dec0519eb20763bc8b6363cb261
Shreya-454 <bshreya932@GMAIL.COM>
2024-02-21 16:20:49 +0530

    updates

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,8 +5,8 @@ const Preloader = () => {
     <div>
       <div className="bg-[#040404] h-screen fixed top-0 left-0 right-0 overflow-hidden flex justify-center items-center">
       <div className="relative flex justify-center items-center">
-    <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-[#02CDCF]"></div>
-    <img src={logo}  className="rounded-full h-28 w-28"/>
+    <div className="absolute animate-spin-slow rounded-full h-36 w-36 border-t-4 border-b-4 border-[#02CDCF]"></div>
+    <img src={logo}  className="rounded-full h-32 w-32"/>
 </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
       backgroundImage:{
         'gradient':'linear-gradient(180deg, #040404 24.07%, rgba(4, 4, 4, 0) 96.3%)'
+      },
+      animation:{
+        'spin-slow':'spin 3s linear infinite'
       }
     },
   },
